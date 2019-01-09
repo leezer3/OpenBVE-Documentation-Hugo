762a4e1d35c11f947523c7bf472d5949c3613b3d
S520 <29241703+s520@users.noreply.github.com>
2019-01-09 14:24:43 +0900

    Update: language.html

--- themes/openbve_documentation/static/script/script.js
+++ themes/openbve_documentation/static/script/script.js
@@ -1,20 +1,9 @@
 $(function() {
     $(document).ready(function() {
         var $select = $('.language-selector').select2({width: 'resolve'});
-        var url = location.href;
-        if (url.search(/\/en\//) !== -1) {
-            $select.val('en').trigger('change');
-        } else if (url.search(/\/ja\//) !== -1) {
-            $select.val('ja').trigger('change');
-        }
     });
 
     $('.language-selector').on('select2:select', function (e) {
-        var url = location.href;
-        if (url.search(/\/en\//) !== -1) {
-            location.href = url.replace(/\/en\//, `/${e.params.data.id}/`);
-        } else if (url.search(/\/ja\//) !== -1) {
-            location.href = url.replace(/\/ja\//, `/${e.params.data.id}/`);
-        }
+        location.href = e.params.data.id;
     });
 });
